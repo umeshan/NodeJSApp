1cd032c9d0541f003b5055decdf10e452da25a8c
umeshan <v.umeshan@gmail.com>
2020-05-31 20:51:27 -0700

    updated dependencies

--- routes/posts.js
+++ routes/posts.js
@@ -1,58 +1,51 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Post = require('../models/Post')
+const Post = require("../models/Post");
 
 // GET POSTS
-router.get('/',async(req,res)=>{
-    try{
-        const dbData = await Post.find();
-        res.json(dbData);
-        console.log(dbData);
-    }
-    catch(err){
-        res.json({message:err});
-    }
+router.get("/", async (req, res) => {
+  try {
+    const dbData = await Post.find();
+    res.json(dbData);
+    console.log(dbData);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
-
 // SUBMITS A POST
-router.post('/',async(req,res)=>{
-    const newPost = new Post({
-        title:req.body.title,
-        description:req.body.description
-
-    });
-    try{
+router.post("/", async (req, res) => {
+  const newPost = new Post({
+    title: req.body.title,
+    description: req.body.description,
+  });
+  try {
     const data = await newPost.save();
     res.json(data);
-
-    }
-    catch(err){
-        res.json({message:err});
-    }
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
 // GET SPECIFIC POST
-router.get('/:postId',async(req,res)=>{
-    const specificData = await Post.findById(req.params.postId);
-    res.json(specificData);
+router.get("/:postId", async (req, res) => {
+  const specificData = await Post.findById(req.params.postId);
+  res.json(specificData);
 });
 
 // UPDATE SPECIFIC POST
-router.patch('/:postId',async(req,res)=>{
-    const updateData = await Post.updateOne(
-        {_id:req.params.postId},
-        {$set:{title:req.body.title}}
-    );
-    res.json(updateData);
+router.patch("/:postId", async (req, res) => {
+  const updateData = await Post.updateOne(
+    { _id: req.params.postId },
+    { $set: { title: req.body.title } }
+  );
+  res.json(updateData);
 });
 
 // DELETE SPECIFIC POST
-router.delete('/:postId',async(req,res)=>{
-    const updateData = await Post.remove(
-        {_id:req.params.postId}
-    );
-    res.json(updateData);
+router.delete("/:postId", async (req, res) => {
+  const updateData = await Post.remove({ _id: req.params.postId });
+  res.json(updateData);
 });
 
 module.exports = router;
